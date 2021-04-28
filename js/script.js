b6dd36b869f5acdb98873001f622c27bc369f659
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-28 15:42:07 +0200

    sviluppo

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@
 // BONUS: Creare una select con tutti i generi dei dischi. 
 // In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
 // BONUS 2: Ordinare i dischi per anno di uscita.
+
+
+
+
+var app = new Vue ({
+    el: '#root',
+
+    data: {
+
+    },
+
+    methods: {
+
+    },
+
+    mounted () {
+
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) => {
+
+                console.log(response);
+            })
+
+    }
+})
